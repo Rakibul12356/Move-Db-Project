86c42c4e485aed1875dce1856e5db71ac02f41d3
Rakibul12356 <mdrakibulhasan12346@gmail.com>
2025-05-20 23:33:50 +0600

    rationg add

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,20 @@ import React from 'react';
 import Header from './Header';
 import SideBar from './SideBar';
 import Footer from './Footer';
+import MovieList from './cine/MovieList';
 
 const App = () => {
   return (
     <>
-      <Header/>
-      <div  className="container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
-        <SideBar/>
-      </div>
-      <Footer/>
+      <Header />
+      <main>
+        <div className="container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
+          <SideBar />
+          <MovieList/>
+        
+        </div>
+      </main>
+      <Footer />
     </>
   );
 };
