184a35ccff3e631de264e02c18d177e16cd6e2db
Rakibul12356 <mdrakibulhasan12346@gmail.com>
2025-05-22 04:23:38 +0600

    Finishied the project

--- src/Page.jsx
+++ src/Page.jsx
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react';
 import Header from './Header';
-import { Toaster } from 'react-hot-toast';
+
 import SideBar from './SideBar';
 import MovieList from './cine/MovieList';
 import Footer from './Footer';
@@ -11,7 +11,7 @@ const Page = () => {
     return (
         <div className={`h-full w-full ${darkMode?"dark":""}`}>
              <Header />
-      <Toaster/>
+     
       <main>
         <div className="container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
           <SideBar />
